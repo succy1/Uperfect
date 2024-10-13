9d1338ee9fd8d5e8e66cfe0f759f6787bec8df4e
succy1 <geometryceptio123@gmail.com>
2024-10-13 17:43:32 +0700

    add static root

--- uperfect1/settings.py
+++ uperfect1/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 
